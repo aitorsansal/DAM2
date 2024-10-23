7006e2432a65b0359c4f0d09d93c9f421f1ffc96
aitorsansal <aitorsansal@gmail.com>
2024-10-23 19:03:35 +0200

    threads project completed

--- MP09/Threads/Threads/Threads/Program.cs
+++ MP09/Threads/Threads/Threads/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Net;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 class Program
 {
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Quin exercici vols fer? 1 o 3?");
+        string input = Console.ReadLine();
+        switch (input)
+        {
+            case "1":
+                Exercici1();
+                break;
+            case "3":
+                Exercici3();
+                break;
+            default:
+                Console.WriteLine("L'element entrat no és cap dels que he demanat");
+                Console.ReadKey();
+                break;
+        }
+
+    }
+
+    #region Ex1
     static int[,] resultMatrix;
     static int[,] matrixA;
     static int[,] matrixB;
     static Thread[] threads;
-    static readonly object lockObject = new object();
-
-    static void Main(string[] args)
+    static readonly object lockObjectEx1 = new object();
+    static void Exercici1()
     {
         // Demanar la mida de les matrius una vegada al principi
         Console.WriteLine("Quina mida de matrius vols (n x n)? Introdueix el valor de n: ");
@@ -55,13 +81,13 @@ class Program
         {
             for (int j = 0; j < cols; j++)
             {
-                Console.Write(resultMatrix[i, j] + " ");
+                Console.Write(resultMatrix[i, j] + "\t");
             }
 
             Console.WriteLine();
         }
     }
-
+    
     // Funció per generar una matriu aleatòria de m x n
     static int[,] GenerateRandomMatrix(int rows, int cols)
     {
@@ -95,7 +121,7 @@ class Program
                     sum += matrixA[i, k] * matrixB[k, j];
                 }
 
-                lock (lockObject)
+                lock (lockObjectEx1)
                 {
                     resultMatrix[i, j] = sum;
                 }
@@ -103,4 +129,77 @@ class Program
         }
 
     }
+
+    #endregion
+
+    #region Ex3
+
+    private const string IMAGE_PATH = "image.jpg";
+    private const string OUTPUT_IMAGE_PATH = "inverted_image.jpg";
+
+    static void Exercici3()
+    {
+        Console.WriteLine("Amb quants fils vols invertir els colors de l'imatge?");
+        int n = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        Bitmap bmp = Bitmap.FromFile(IMAGE_PATH) as Bitmap;
+
+        var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+        var data = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+        var depth = Bitmap.GetPixelFormatSize(data.PixelFormat) / 8;
+
+        var buffer = new byte[data.Width * data.Height * depth];
+
+        // Copy pixels to buffer
+        Marshal.Copy(data.Scan0, buffer, 0, buffer.Length);
+
+
+        Thread[] threads = new Thread[n];
+        int quantPerTask = data.Height / n;
+
+        for (int i = 0; i < n; i++)
+        {
+            int startY = i * quantPerTask;
+            int endY = (i == n - 1) ? data.Height : startY + quantPerTask; // Handle last thread
+
+            threads[i] = new Thread(() => Process(buffer, startY, endY, data.Width, depth));
+            threads[i].Start();
+        }
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+        
+        Marshal.Copy(buffer, 0, data.Scan0, buffer.Length);
+        bmp.UnlockBits(data);
+        bmp.Save(OUTPUT_IMAGE_PATH, ImageFormat.Jpeg);
+        stopwatch.Stop();
+        
+        double elapsedMilliseconds = stopwatch.ElapsedTicks * 1000.0 / Stopwatch.Frequency; // Converteix a mil·lisegons
+
+        Console.WriteLine($"Temps invertir colors: {elapsedMilliseconds} ms\n");
+        
+
+        Console.WriteLine("Saved image");
+    }
+    
+    static void Process(byte[] buffer, int startY, int endY, int width, int depth)
+    {
+        for (int j = startY; j < endY; j++)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                var offset = ((j * width) + i) * depth;
+
+                // Invert colors: 255 - original color value
+                buffer[offset + 0] = (byte)(255 - buffer[offset + 0]); // Invert Red
+                buffer[offset + 1] = (byte)(255 - buffer[offset + 1]); // Invert Green
+                buffer[offset + 2] = (byte)(255 - buffer[offset + 2]); // Invert Blue
+            }
+        }
+    }
+
+    #endregion
 }
