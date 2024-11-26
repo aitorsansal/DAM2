9d9f00e3e02743f252ae8f2b443bd9721d614b11
aitorsansal <aitorsansal@gmail.com>
2024-11-26 20:45:00 +0100

    finished viewmodel tut

---
+++ MP07/WPF/ClientsMVVM - Starter/ClientsMVVM/ViewModel/ClientViewModel.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using ClientsMVVM.Model;
+using ClientsMVVM.Repositiori;
+
+namespace ClientsMVVM.ViewModel;
+
+public class ClientViewModel : ObservableBase
+{
+    #region Fields
+
+    private string name;
+    private string surname;
+    private string photo;
+    private int position = -1;
+    private string paycheck;
+    private bool isValid;
+    private bool editing;
+    private ObservableCollection<Client> clients = new();
+    IRepositoriDeClients clientsRepository = Repositori.ObreBDClients();
+    #endregion
+    
+    #region Properties
+    public string Name
+    {
+        get => name;
+        set
+        {
+            SetProperty(ref name, value);
+            NotifyPropertyChanged(nameof(CompleteName));
+            ValidateData();
+        }
+    }
+
+    public string Surname
+    {
+        get => surname;
+        set
+        {
+            SetProperty(ref surname, value);
+            NotifyPropertyChanged(nameof(CompleteName));
+            ValidateData();
+        }
+    }
+
+    public bool IsValid
+    {
+        get => isValid;
+        set => SetProperty(ref isValid, value);
+    }
+
+    public bool Editing
+    {
+        get => editing;
+        set => SetProperty(ref editing, value);
+    }
+
+    public ObservableCollection<Client> Clients
+    {
+        get => clients;
+        set => SetProperty(ref clients, value);
+    }
+
+    public string Paycheck
+    {
+        get => paycheck;
+        set
+        {
+            SetProperty(ref paycheck, value);
+            ValidateData();
+        }
+    }
+
+    public int Position
+    {
+        get => position;
+        set => SetProperty(ref position, value);
+    }
+    public string CompleteName => Name + " " + Surname;
+
+    #endregion
+
+    #region Command Properties
+
+    public ICommand CreateClientsCommand { get; set; }
+    public ICommand AddClientCommand { get; set; }
+    public ICommand EditClientCommand { get; set; }
+    public ICommand ConfirmClientCommand { get; set; }
+    public ICommand DismissClientCommand { get; set; }
+    public ICommand DeleteClientCommand { get; set; }
+    
+
+    #endregion
+
+    void ValidateData()
+    {
+        IsValid = !string.IsNullOrEmpty(Name)
+                  && !string.IsNullOrEmpty(Surname)
+                  && decimal.TryParse(Paycheck, out _);
+    }
+
+
+    public ClientViewModel()
+    {
+        clients = clientsRepository.Obten();
+        Position = -1;
+
+        #region Commands
+
+        CreateClientsCommand = new RelayCommand<string>(CreateClients, _ => Clients.Count == 0);
+        AddClientCommand = new RelayCommand(AddClient, _ => IsValid && !Editing);
+        EditClientCommand = new RelayCommand(ActivateEditing, _ => !Editing && Position != -1);
+        ConfirmClientCommand = new RelayCommand(ConfirmEditingClient, _ => Editing && IsValid);
+        DismissClientCommand = new RelayCommand(DismissEditingClient, _ => Editing);
+        DeleteClientCommand = new RelayCommand(DeleteClient, _ => Position != -1);
+
+        #endregion
+    }
+
+    private void CreateClients(string nClients)
+    {
+        if (int.TryParse(nClients, out var clients))
+        {
+            clientsRepository.CreaClients(clients);
+            Clients = clientsRepository.Obten();
+        }
+    }
+    private void AddClient(object obj)
+    {
+        string id = Guid.NewGuid().ToString();
+        Client newClient = new()
+        {
+            Nom = Name,
+            Cognom = Surname,
+            Saldo = decimal.Parse(Paycheck),
+            Foto = $"https://loremflickr.com/320/240/portrait,{Name}?lock={id}",
+            Id = id
+        };
+        clientsRepository.Afegeix(newClient);
+        Clients = clientsRepository.Obten();
+        Name = "";
+        Surname = "";
+        Paycheck = "0";
+    }
+    private void ActivateEditing(object obj)
+    {
+        Name = Clients[Position].Nom;
+        Surname = Clients[Position].Cognom;
+        Paycheck = Clients[Position].Saldo.ToString();
+        Editing = true;
+    }
+    private void ConfirmEditingClient(object obj)
+    {
+        Client newClient = new()
+        {
+            Id = Clients[Position].Id,
+            Nom = Name,
+            Cognom = Surname,
+            Saldo = decimal.Parse(Paycheck),
+            Foto = Clients[Position].Foto
+        };
+        clientsRepository.Modifica(newClient);
+        Clients = clientsRepository.Obten();
+        Editing = false;
+        Name = "";
+        Surname = "";
+        Paycheck = "0";
+    }
+    private void DismissEditingClient(object obj)
+    {
+        Editing = false;
+        Name = "";
+        Surname = "";
+        Paycheck = "0";
+    }
+    private void DeleteClient(object obj)
+    {
+        clientsRepository.Esborra(Clients[Position].Id);
+        Clients = clientsRepository.Obten();
+    }
+}
---
+++ MP07/WPF/ClientsMVVM - Starter/ClientsMVVM/ViewModel/ClientViewModelCommunityToolkit.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using ClientsMVVM.Model;
+using ClientsMVVM.Repositiori;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ClientsMVVM.ViewModel;
+
+public partial class ClientViewModelCommunityToolkit : ObservableObject
+{
+    #region Fields
+
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(CompleteName)), NotifyPropertyChangedFor(nameof(IsValid)), 
+     NotifyCanExecuteChangedFor(nameof(AddClientCommand)), NotifyCanExecuteChangedFor(nameof(ConfirmClientCommand))]
+    private string name;
+    partial void OnNameChanged(string value)
+    {
+        ValidateData();
+    }
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(CompleteName)), NotifyPropertyChangedFor(nameof(IsValid)), 
+     NotifyCanExecuteChangedFor(nameof(AddClientCommand)), NotifyCanExecuteChangedFor(nameof(ConfirmClientCommand))]
+    private string surname;
+    partial void OnSurnameChanged(string value)
+    {
+        ValidateData();
+    }
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(IsValid)), 
+     NotifyCanExecuteChangedFor(nameof(AddClientCommand)), NotifyCanExecuteChangedFor(nameof(ConfirmClientCommand))]
+    private string paycheck;
+    partial void OnPaycheckChanged(string value)
+    {
+        ValidateData();
+    }
+    [ObservableProperty, 
+     NotifyCanExecuteChangedFor(nameof(EditClientCommand)), NotifyCanExecuteChangedFor(nameof(DeleteClientCommand)), NotifyCanExecuteChangedFor(nameof(CreateClientsCommand))]
+    private int position = -1;
+    [ObservableProperty, NotifyCanExecuteChangedFor(nameof(AddClientCommand)), NotifyCanExecuteChangedFor(nameof(ConfirmClientCommand))]
+    private bool isValid;
+    [ObservableProperty, 
+     NotifyCanExecuteChangedFor(nameof(AddClientCommand)), NotifyCanExecuteChangedFor(nameof(ConfirmClientCommand))]
+    [NotifyCanExecuteChangedFor(nameof(DismissClientCommand)), NotifyCanExecuteChangedFor(nameof(EditClientCommand))]
+    private bool editing;
+    [ObservableProperty]
+    private ObservableCollection<Client> clients;
+
+    readonly IRepositoriDeClients clientsRepository = Repositori.ObreBDClients();
+    #endregion
+    
+    public string CompleteName => Name + " " + Surname;
+
+    public ClientViewModelCommunityToolkit()
+    {
+        clients = clientsRepository.Obten();
+        Position = -1;
+    }
+    
+    #region Voids
+
+    void ValidateData()
+    {
+        IsValid = !string.IsNullOrEmpty(Name)
+                  && !string.IsNullOrEmpty(Surname)
+                  && decimal.TryParse(Paycheck, out _);
+    }
+    [RelayCommand (CanExecute = nameof(CanCreateClients))]
+    private void CreateClients(string nClients)
+    {
+        if (int.TryParse(nClients, out var quantity))
+        {
+            clientsRepository.CreaClients(quantity);
+            Clients = clientsRepository.Obten();
+        }
+    }
+    
+    [RelayCommand (CanExecute = nameof(CannAddClient))]
+    private void AddClient()
+    {
+        string id = Guid.NewGuid().ToString();
+        Client newClient = new()
+        {
+            Nom = Name,
+            Cognom = Surname,
+            Saldo = decimal.Parse(Paycheck),
+            Foto = $"https://loremflickr.com/320/240/portrait,{Name}?lock={id}",
+            Id = id
+        };
+        clientsRepository.Afegeix(newClient);
+        Clients = clientsRepository.Obten();
+        Name = "";
+        Surname = "";
+        Paycheck = "0";
+    }
+
+    
+    [RelayCommand (CanExecute = nameof(CanActivateEditing))]
+    private void EditClient()
+    {
+        Name = Clients[Position].Nom;
+        Surname = Clients[Position].Cognom;
+        Paycheck = Clients[Position].Saldo.ToString();
+        Editing = true;
+    }
+
+    
+    [RelayCommand (CanExecute = nameof(CanConfirmEditingClient))]
+    private void ConfirmClient()
+    {
+        Client newClient = new()
+        {
+            Id = Clients[Position].Id,
+            Nom = Name,
+            Cognom = Surname,
+            Saldo = decimal.Parse(Paycheck),
+            Foto = Clients[Position].Foto
+        };
+        clientsRepository.Modifica(newClient);
+        Clients = clientsRepository.Obten();
+        Editing = false;
+        Name = "";
+        Surname = "";
+        Paycheck = "0";
+    }
+
+    
+    [RelayCommand (CanExecute = nameof(CanDissmissEditinClient))]
+    private void DismissClient()
+    {
+        Editing = false;
+        Name = "";
+        Surname = "";
+        Paycheck = "0";
+    }
+
+    [RelayCommand (CanExecute = nameof(CanDeleteClient))]
+    private void DeleteClient()
+    {
+        clientsRepository.Esborra(Clients[Position].Id);
+        Clients = clientsRepository.Obten();
+    }
+
+    private bool CanCreateClients() => Clients.Count == 0;
+    private bool CannAddClient() => IsValid && !Editing;
+    bool CanActivateEditing() => Position != -1 && !Editing;
+    private bool CanConfirmEditingClient() => Editing && IsValid;
+    private bool CanDissmissEditinClient() => Editing;
+    bool CanDeleteClient() => Position != -1;
+
+    #endregion
+}
